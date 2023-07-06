37405616e65cc1f6f58668d1630bc350ee77831b
kruskal <99559985+kruskall@users.noreply.github.com>
2023-07-06 09:43:02 +0200

    fix: update struct name

--- bulk_indexer.go
+++ bulk_indexer.go
@@ -57,8 +57,8 @@ type bulkIndexer struct {
 	resp         MinimalBulkIndexerResponse
 }
 
-// BulkIndexerResponse represents a minimal subset of the Elasticsearch _bulk API response.
-type BulkIndexerResponse struct {
+// MinimalBulkIndexerResponse represents a minimal subset of the Elasticsearch _bulk API response.
+type MinimalBulkIndexerResponse struct {
 	Items []map[string]BulkIndexerResponseItem `json:"items,omitempty"`
 }
 
