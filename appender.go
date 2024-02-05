cfb8144724946832836ee9f4b995d374898353b8
kruskall <99559985+kruskall@users.noreply.github.com>
2024-02-05 10:32:19 +0100

    feat: return an error when using small flush bytes with compression enabled (#111)
    * feat: return an error when using small flush bytes with compression enabled
    
    When compression is enabled the gzip writer will buffer data internally
    which will cause the unerlying buffer to grow from 0 to n once compression
    happens and compressed data are flushed.
    Because of this, small flush bytes values are ignored and will never be hit.
    The PR returns an error when using a small flush bytes value with compression
    enabled.
    
    * refactor: lower minimum to 16kb and update error message

--- appender.go
+++ appender.go
@@ -127,6 +127,14 @@ func New(client *elasticsearch.Client, cfg Config) (*Appender, error) {
 		}
 	}
 
+	minFlushBytes := 16 * 1024 // 16kb
+	if cfg.CompressionLevel != 0 && cfg.FlushBytes < minFlushBytes {
+		return nil, fmt.Errorf(
+			"flush bytes config value (%d) is too small and will be ignored with compression enabled. Use at least %d",
+			cfg.FlushBytes, minFlushBytes,
+		)
+	}
+
 	ms, err := newMetrics(cfg)
 	if err != nil {
 		return nil, err
