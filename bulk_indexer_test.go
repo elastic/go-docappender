10872d1f8d50014fbf8cac099161a1be8dfc40f7
Vishal Raj <vishal.raj@elastic.co>
2024-04-23 12:52:39 +0100

    Fix allocation the document retry buffer (#160)

---
+++ bulk_indexer_test.go
@@ -0,0 +1,113 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package docappender_test
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/klauspost/compress/gzip"
+
+	"github.com/elastic/go-docappender/v2"
+	"github.com/elastic/go-docappender/v2/docappendertest"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBulkIndexer(t *testing.T) {
+	for _, tc := range []struct {
+		Name             string
+		CompressionLevel int
+	}{
+		{Name: "no_compression", CompressionLevel: gzip.NoCompression},
+		{Name: "most_compression", CompressionLevel: gzip.BestCompression},
+		{Name: "speed_compression", CompressionLevel: gzip.BestSpeed},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			var esFailing atomic.Bool
+			client := docappendertest.NewMockElasticsearchClient(t, func(w http.ResponseWriter, r *http.Request) {
+				_, result := docappendertest.DecodeBulkRequest(r)
+				if esFailing.Load() {
+					for _, itemsMap := range result.Items {
+						for k, item := range itemsMap {
+							result.HasErrors = true
+							item.Status = http.StatusTooManyRequests
+							item.Error.Type = "simulated_es_error"
+							item.Error.Reason = "for testing"
+							itemsMap[k] = item
+						}
+					}
+				}
+				json.NewEncoder(w).Encode(result)
+			})
+			indexer, err := docappender.NewBulkIndexer(docappender.BulkIndexerConfig{
+				Client:                client,
+				MaxDocumentRetries:    100_000, // infinite for testing purpose
+				RetryOnDocumentStatus: []int{http.StatusTooManyRequests},
+				CompressionLevel:      tc.CompressionLevel,
+			})
+			require.NoError(t, err)
+
+			generateLoad := func(count int) {
+				for i := 0; i < count; i++ {
+					require.NoError(t, indexer.Add(docappender.BulkIndexerItem{
+						Index: "testidx",
+						Body: newJSONReader(map[string]any{
+							"@timestamp": time.Now().Format(docappendertest.TimestampFormat),
+						}),
+					}))
+				}
+			}
+
+			itemCount := 1_000
+			generateLoad(itemCount)
+
+			// All items should be successfully flushed
+			stat, err := indexer.Flush(context.Background())
+			require.NoError(t, err)
+			require.Equal(t, int64(itemCount), stat.Indexed)
+
+			// Simulate ES failure, all items should be enqueued for retries
+			esFailing.Store(true)
+			generateLoad(itemCount)
+			require.Equal(t, itemCount, indexer.Items())
+
+			for i := 0; i < 10; i++ {
+				stat, err := indexer.Flush(context.Background())
+				require.NoError(t, err)
+				require.Equal(t, int64(0), stat.Indexed)
+				require.Equal(t, itemCount, len(stat.FailedDocs))
+				require.Equal(t, int64(itemCount), stat.RetriedDocs)
+
+				// Generate more load, all these items should be enqueued for retries
+				generateLoad(10)
+				itemCount += 10
+				require.Equal(t, itemCount, indexer.Items())
+			}
+
+			// Recover ES and ensure all items are indexed
+			esFailing.Store(false)
+			stat, err = indexer.Flush(context.Background())
+			require.NoError(t, err)
+			require.Equal(t, int64(itemCount), stat.Indexed)
+		})
+	}
+}
