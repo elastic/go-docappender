2a0efd737ab9f43da3d2f6e80f5ee44fb4dadecf
Carson Ip <carson.ip@elastic.co>
2024-06-28 15:12:11 +0100

    Print bytes

--- bulk_indexer.go
+++ bulk_indexer.go
@@ -321,6 +321,8 @@ func (b *BulkIndexer) Flush(ctx context.Context) (BulkIndexerResponseStat, error
 		copy(b.copyBuf, b.buf.Bytes())
 	}
 
+	fmt.Println(b.buf.Bytes())
+
 	req := esapi.BulkRequest{
 		// We should not pass the original b.buf bytes.Buffer down to the client/http layer because
 		// the indexer will reuse the buffer. The underlying http client/transport implementation may keep
